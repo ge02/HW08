d6501ad109d375a4125104e13580edb52dd628fa
ge02 <105367725+ge02@users.noreply.github.com>
2023-01-09 10:01:17 +0100

    Update Calculator.java

--- hw8/src/hw_8_task1/Calculator.java
+++ hw8/src/hw_8_task1/Calculator.java
@@ -2,18 +2,25 @@ package hw_8_task1;
 
 public class Calculator {
 
+	int x = 3;
+	boolean isTrue = true;
+
+	// plus operation
 	public int add(int valueOne, int valueTwo) {
 		return valueOne + valueTwo;
 	}
 
+	// minus operation
 	public int sub(int valueOne, int valueTwo) {
 		return valueOne - valueTwo;
 	}
 
+	// multiply operation
 	public int multiply(int valueOne, int valueTwo) {
 		return valueOne * valueTwo;
 	}
 
+	// this part works correct
 	public float divide(int numerator, int denominator) {
 		if (denominator == 0)
 			throw new ArithmeticException("ZeroDivisionError");
@@ -21,5 +28,8 @@ public class Calculator {
 		return (float) numerator / denominator;
 
 	}
-
+	
+	if(true){
+		System.out.println("true is true");
+	}
 }
